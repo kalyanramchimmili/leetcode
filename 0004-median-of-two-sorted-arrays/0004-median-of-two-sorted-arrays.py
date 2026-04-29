c3646fa5a1877a3c626b1bacea1e2c60e23d09b1
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-04-29 14:13:11 +0530

    Update 0004-median-of-two-sorted-arrays.py

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,3 +1,9 @@
+"""
+1. Find the median, was pretty direct - combine the l2 with l1 and sort the array
+2. the usual check of it is odd then return middle number, if not avg of mid 2 numbers
+3. First solution was num1.append but it would add [1,2,[3,4]], then changed it to extend, which solved the issue
+4. combining concatenation and sorting saved me around 4ms in solution, beating 100 percentile, nums = sorted(nums1+nums2) which is pretty cool 
+"""
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         #nums1.extend(nums2)
