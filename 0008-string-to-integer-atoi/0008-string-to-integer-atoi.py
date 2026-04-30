5682ba63b2dcb7b31ec5238538ba587c7964adde
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-04-30 20:58:53 +0530

    Time: 1 ms (48.05%), Space: 19.1 MB (96.3%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,3 +1,12 @@
+"""
+1. check for leading whitestrips, if the ans string is empty then, continuie if not break
+2. check for signs plus and neg, if the string ans is empty add else break
+3. check if we encounter any non-digit, then break
+4. else other numbers we add to char i.e., numbers
+5. Check if ans is empty is yes return 0 else convert it into a number
+6. check if it is less than 2^31-1 or greater than -2^31 then return the rounded of values
+7. return the ans otherwise
+"""
 class Solution:
     def myAtoi(self, s: str) -> int:
         ans = ""
