0719dd3dbf246e19ebd21597771f35444591aa98
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-04-30 18:39:09 +0530

    Time: 30 ms (99.65%), Space: 19 MB (92.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,3 +1,9 @@
+"""
+This is quite simple
+1. the std reversal of number, get the last digit out, mul the prev rev into 10 and add the rem we get until the orginal number becomes 0
+2. for neg numbers, have a flag, mul with -1 first and after reversing mul with -1 again as per que
+3. if the reversal overflows return 0 checking with the  if reverse < -2**31 or reverse > (2**31 - 1):
+"""
 class Solution:
     def reverse(self, x: int) -> int:
 
