ac0cf3ed1281ddc0cdc64c29e453ff6649d49005
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-04 17:26:27 +0530

    Time: 12 ms (9.26%), Space: 19.2 MB (64.48%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        conv_map = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+   
+        ans = ""
+        for value, sym in conv_map:
+            if num != 0:
+                temp = num // value
+                ans += sym*temp
+                num -= temp*value
+            else:
+                break
+        
+        return ans
+
+       
