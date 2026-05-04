37b1831fac80efe5dec8054425b18283bab3652e
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-04 16:36:53 +0530

    Time: 11 ms (37.08%), Space: 19.2 MB (86.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,3 +1,12 @@
+"""
+It was similar to reverse.
+1. As per problem description, negative numbers with sign are not palindromes as the sign comes post the int after reversing it
+2. 0-9 are all palindromes
+3. was to rev a int with tmp variable and check if its a palindrome
+
+time comp:- O(log10(x)) // number of digits in x
+space comp:- o(1) //just 2 constants
+"""
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
