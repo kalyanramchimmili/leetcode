27b307b7243fc8aae94bdb009b9cc47b7cb83cca
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-04 18:55:33 +0530

    Time: 3 ms (80.97%), Space: 19.5 MB (19.86%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,3 +1,14 @@
+"""
+1. similar to previous problem, converting roman to int
+2. list out dict as hashmap, iterate through the string 
+3. to check for 4 and 9s case if next char is bigger than prev char it is either 4 or 9, hence subtract the present value and add next value
+for eg:- 
+IV :- sub 1 and add 5 so it results in 4
+5. else add it to the ans int and return the ans
+
+time comp:- o(n) -> no of char in str of len n
+space comp:- o(1)
+"""
 class Solution:
     def romanToInt(self, s: str) -> int:
         conv_map = {
