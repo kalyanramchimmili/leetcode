160096572f719cad0a7831f6546c9b0406d1cf5b
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-04 17:30:58 +0530

    Time: 5 ms (50.07%), Space: 19.5 MB (6.62%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,3 +1,11 @@
+"""
+1. map a dict whichs serves as hash map, instead of handling 4 and 9's map those at as well
+2. from first value and its symbol, first do a floor of num and value to get how many times of value is the number and add the symbol that many times to ans string
+3. Modify the num by over-writing it with reminder and continue until it is not 0
+
+time comp:- o(1) // runs 13 times (13 entires in dict)
+space comp:- o(1)
+"""
 class Solution:
     def intToRoman(self, num: int) -> str:
         conv_map = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
@@ -7,7 +15,7 @@ class Solution:
             if num != 0:
                 temp = num // value
                 ans += sym*temp
-                num %= value
+                num %= value #similar to num = num - (temp*value)
             else:
                 break
         
