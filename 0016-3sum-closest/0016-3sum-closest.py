308bfc088037aad52ab6e0a312567711b1013346
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-05 18:48:26 +0530

    Time: 390 ms (42.89%), Space: 19.3 MB (83.7%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,3 +1,13 @@
+"""
+1. similar to the prev 3-sum problem
+2. one fixed var and a left and right pointer, sort the array to begin with and get the sum of min 3 elements as a baseline
+3. for fixed from 0 to n-2, if the sum is equal to target best match return the target or curr_sum
+4. if the diff btw the curr sum and target is lower than ans then replace ans with curr sum.
+5. if the sum is grater than target dec right else inc left, return the final ans
+
+time comp:- O(n^2)
+space comp:- O(1)
+"""
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         n = len(nums)
