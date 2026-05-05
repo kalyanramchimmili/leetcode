8775bacf8343315a391171421f8c6bcd48a162a7
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-05 18:31:23 +0530

    Time: 494 ms (89%), Space: 22.1 MB (93.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,11 +1,12 @@
 """
-1. This was the famous 3-sum problem, it was a bit tough, specially skipping negatives
-2. we are taking one fixed pointer from o to n-2 len, we have left which is fixed +1 and right at last element (one fixed element and 2 pointer method)
-3. sort the array before we began, if sum of all 3 pos is > 0 right -- if < 0 then left ++, else append those triplet to ans and dec right and inc left
-4. to avoid duplicates, the top loop if fixed pos > 0 and the prev fixed int is same as present one, skip it to not add those values again
-(for eg:- [-1, -1, 0, 1]. shld be -1, 0, 1 so skip if the prev value is same in cases such as this)
+1. This was the famous 3-sum problem, it was a bit tough, specially skipping negatives.
+2. we are taking one fixed pointer from o to n-2 len, we have left which is fixed +1 and right at last element (one fixed element and 2 pointer method).
+3. sort the array before we began, if sum of all 3 pos is > 0 right -- if < 0 then left ++, else append those triplet to ans and dec right and inc left.
+4. to avoid duplicates, the top loop if fixed pos > 0 and the prev fixed int is same as present one, skip it to not add those values again.
+(for eg:- [-1, -1, 0, 1]. shld be -1, 0, 1 so skip if the prev value is same in cases such as this).
 4.1 a second while loop to skip the duplicated for [1,2,0,1,0,0,0,0], if left is same as prev value and is less than right skip that left by inc the left.
-5. return the ans
+4.2 if the smallest value it is fixed greater than 0 then no way the sum is o break and return the value.
+5. return the ans.
 
 time comp:- o(n^2)
 space comp:- o(1)
