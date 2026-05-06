1b8e97d3723a8ae6ef77422ab1109250f0a3cd7d
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-06 22:03:24 +0530

    Update 0020-valid-parentheses.py

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,3 +1,11 @@
+"""
+1. Stack solution, if opening brackets push it in stack, if not check the closing brackets match with opening ones and stack is not empty, if so stack.pop
+
+2. if any mismatch with brackets then return False, check if stack is empty at the end, if so pass true else false
+
+time comp:- O(N)
+space comp:- O(N)
+"""
 class Solution:
     def isValid(self, char: str) -> bool:
         stack = []
@@ -13,7 +21,5 @@ class Solution:
                     stack.pop()
                 else:
                     return False
-        if not stack:
-            return True
-        else:
-            return False
+
+        return not stack
