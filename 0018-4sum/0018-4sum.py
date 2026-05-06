30fdb2378eced610f54534c0af4cad4db452289c
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-06 19:29:58 +0530

    Time: 565 ms (14.8%), Space: 19.3 MB (85.82%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,3 +1,13 @@
+"""
+1. waiting for leetcode to drop 5-sum next, but coming to solution still the 2 pointer solution with 2 fixed values and 2 pointers
+2. 2 for loops with f1 and f2 as 2 fixed values, left and right pointers, the que is bit confusing, but they want unique indices and values. The framing of que is stupid
+3. first loop iterates from 0 to l-3 and 2nd loop 0 to l-2, during each loop we have left and right var, if sum is equal to target append the list to ans list, then inc left and right. To skip duplicates, check if prev left and inc left is same if yes inc, and same for right if prev right and dec right is same, dec right again.
+4. same logic if sum is less than target, then inc left until its prev value is not same and same for right keep dec right is sum is greater than target
+5. to skip duplicates in top, if f1 is > 0 and if it is same as prev one skip, same for f2 is greater than starting pos and prev pos is same skip that pos
+
+time comp:- 2 loops n-3*n-2 and each loop is doing n-2 ~ O(n^3)
+space comp:- o(1)
+"""
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         l = len(nums)
