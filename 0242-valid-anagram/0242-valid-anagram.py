2342d71714c8f202f34ca38a5b006f7dfa77c4a3
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-07 18:12:31 +0530

    Time: 15 ms (42.9%), Space: 19.9 MB (21.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,9 +8,6 @@ space comp:- o(2n)
 """
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s):
-            return False
-            
         sorted_s = "".join(sorted(s))
         sorted_t = "".join(sorted(t))
         return (sorted_s == sorted_t)
