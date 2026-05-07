45a483ee9ee65cfd605435414b3aff0fc319519d
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-07 18:10:55 +0530

    Time: 11 ms (33.37%), Space: 19.6 MB (56.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,3 +1,12 @@
+"""
+1. 2 pointers method left and right starting from first and last respectively
+2. if left or right char is not a char determine by isalnum which will check if it is char or num, if not skip
+3. if yes compare left and right if not return false, else inc left and dec right
+4. last return true if the loop passes
+
+time comp:- o(n)
+space :- o(1)
+"""
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         n = len(s)
