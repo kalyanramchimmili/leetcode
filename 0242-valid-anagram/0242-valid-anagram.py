ff789f3364d92391125d60028985ab3c60c9991f
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-07 18:12:17 +0530

    Time: 22 ms (5.34%), Space: 19.8 MB (21.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,16 @@
+"""
+1. sort the 2 strings using sorted which would split the string into list and sort them based on ascii
+2. use join to form string
+3. sort both s and t compare them and return the output
+
+time comp:- o(2nlogn) assuming n is len of t and n
+space comp:- o(2n)
+"""
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(t) != len(s):
+            return False
+            
         sorted_s = "".join(sorted(s))
         sorted_t = "".join(sorted(t))
         return (sorted_s == sorted_t)
