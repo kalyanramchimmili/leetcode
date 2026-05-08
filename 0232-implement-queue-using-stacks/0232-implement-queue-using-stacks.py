4d9126e4fad65cc70b16698b28018959d65f6be1
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-08 19:55:33 +0530

    Time: 0 ms (100%), Space: 19.6 MB (14.66%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,3 +1,12 @@
+"""
+1. 2 stacks for a queue, to push, push it to stack 1
+2. to pop, push all elements of stack 1 to stack 2, pop the stack 2, store the values, and shift all the elements back from stack 2 to stack 1, and return the value
+3. for peek return stack[0] the first element in stack
+4. for empty, check the size of stack 1, return true or false
+
+time comp:- O(n)
+space:- O(n)
+"""
 class MyQueue:
 
     def __init__(self):
