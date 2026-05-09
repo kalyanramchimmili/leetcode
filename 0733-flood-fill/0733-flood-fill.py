67f6a824578dcc4a7162793475ab86b1bd8e9e0d
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-09 19:27:18 +0530

    Time: 0 ms (100%), Space: 19.8 MB (31.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,3 +1,13 @@
+"""
+1. this one was pretty hard to understand, but it was simple after that via recurrsion
+2. capture the orignal value, if the value is already same as colour, nothing to change, return the image
+3. we have fill fun within, which checks the boundary for rows and columns, if the new index is equal to orignal value or not, if not or out of index return simply.
+4. if not then change it to colour, do the same thing for up down, right left 
+5. start the fun by first calling the given index and return the image after it
+
+time comp:- O(m*n)
+space comp:- O(m*n)
+"""
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
