9eca1e09e80dca072fdcaa7d5489ca5b97a26ec2
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-09 20:11:15 +0530

    Time: 39 ms (86.85%), Space: 19.3 MB (25.07%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -1,3 +1,13 @@
+"""
+1. running a linear search didnt work, TLE
+2. used binary search for this, if the mid is bad then mid could be the first bad version or previous to mid
+3. if mid is not bad, then it should be from mid+1 to n
+4. calculate new mid check if it is bad until left < right
+5. return left, for n==1 the while would break and return left directly which is 1
+
+time comp:- O(logn)
+space comp :- O(1)
+"""
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
