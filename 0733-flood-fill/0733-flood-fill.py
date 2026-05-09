4f65ad102add977a70ba5ac71b2a1ce8fab75767
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-09 19:22:04 +0530

    Time: 4 ms (4.51%), Space: 19.7 MB (42.47%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,29 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        orginal_value = image[sr][sc]
+
+        if orginal_value == color:
+            return image
+
+        def fill(r, c):
+            if r < 0 or c < 0 or r >= len(image) or c >= len(image[0]) or (image[r][c] != orginal_value):
+                return
+
+            image[r][c] = color
+
+            fill(r-1, c)
+            fill(r+1, c)
+            fill(r, c-1)
+            fill(r, c+1)
+        
+        fill(sr,sc)
+        return image
+
+        
+
+
+
+
+        
+        
