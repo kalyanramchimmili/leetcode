fb2ebb6db43259b07ffb77ca611f6be56e761779
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-09 21:47:59 +0530

    Time: 56 ms (5.17%), Space: 19.6 MB (72.78%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,19 @@
+"""
+1. intialize count var and count all char in magazine
+2. check in ransomNote if there is any char not in count or if its frew has got to 0, but the char exists in ransomNote, return false
+3. dec the count of the ch in count other wise 
+4. if loop completes, ransomNote can be made form magazine, return true
+
+time comp:- O(N), n = len(ransomNote)
+space comp:- O(N)
+"""
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+
         count = {}
+        
         for char in magazine:
             count[char] = count.get(char,0)+1
         
