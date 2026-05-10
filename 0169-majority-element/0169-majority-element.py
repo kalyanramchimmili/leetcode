fd4b9e5ce5c6a8aaa1a4900fec3eb69f04614b72
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-10 22:36:59 +0530

    Time: 15 ms (19.27%), Space: 21 MB (98.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,18 @@
+"""
+1. similar to ransom note problem, have a count hashmap
+2. inc the count, if count of an element is more than half of the list, then return the num
+3. as per que there is majority element always hence no need to handle any other case
+
+time comp:- O(N)
+space comp:- O(N)
+"""
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = {}
         l = len(nums)//2
         for i in nums:
-            temp = count.get(i) if count.get(i) else 0
-            count[i] = temp + 1
-            if temp >= l:
+            count[i] = count.get(i,0) + 1
+            if count[i] > l:
                 return i
 
         
