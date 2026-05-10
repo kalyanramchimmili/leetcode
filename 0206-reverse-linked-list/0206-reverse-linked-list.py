1c15ae9e1a544361b78cc8699749fc4e42895e59
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-10 21:39:40 +0530

    Time: 0 ms (100%), Space: 20.6 MB (7.66%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,16 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+"""
+1. 3 pointers, 1st pointer is ref node to reverse to, 2nd pointer is the current element to reverse, 3rd element is next element to traverse to
+2. start with null, head, head for ptr1 , 2 ,3 
+3. if ptr3 is true, move ptr3 to next, ptr2.next to ptr1, ptr1 would be ptr2 and ptr2 would go to ptr3 pos
+4. to return head return ptr1, ptr2 and 3 would have gone to null when checking for last node.
+
+time comp:- O(N)
+space comp:- O(1)
+"""
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         ptr1 = None
