c0b7de02eabaac0e4020aefe25790d82492e70c9
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-11 18:28:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (19.17%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,3 +1,10 @@
+"""
+1. fast and slow pointer method, move fast pointer to 2 steps and slow to one
+2. for even ll, fast would be at n-1 node, so if fast.next exists, then move slow one step further and return slow
+
+time comp:- O(N)
+space comp:- O(1)
+"""
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -9,7 +16,7 @@ class Solution:
         while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
-            
+
         if fast.next:
             slow = slow.next
         
