872c1906d95d4104b3ac35e8e58ce36fbed63bcb
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-11 18:19:41 +0530

    Time: 0 ms (100%), Space: 22.2 MB (35.08%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -0,0 +1,32 @@
+"""
+1. Define height fun from balanced binary tree method
+2. Intialize a diameter to find the max diameter for left+right.
+3. Return the max diameter recorded
+
+time comp:- O(N)
+space comp:- O(h) -> recursive track.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
+        
+        def height(node):
+            nonlocal diameter
+
+            if node is None:
+                return 0
+
+            leftHeight = height(node.left)
+            rightHeight = height(node.right)
+            diameter = max(diameter, leftHeight+rightHeight)
+
+            return 1+max(leftHeight,rightHeight)
+        
+        height(root)
+        return diameter
