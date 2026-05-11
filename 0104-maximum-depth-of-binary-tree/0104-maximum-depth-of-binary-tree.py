3db7925dee6d04a6ec839eda7b0a735b51a8fd4d
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-11 18:36:42 +0530

    Time: 1 ms (31.79%), Space: 20.2 MB (91.73%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,3 +1,11 @@
+"""
+1. similar to diameter or balanced binary tree, find the max height of a tree from node
+2. make a height fun for left and right node, return 1+max(left,hight) would give max height of left or right and add in recursive way
+3. return the height from node
+
+time comp:- O(N)
+space comp:- O(h) - recusive stack
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
