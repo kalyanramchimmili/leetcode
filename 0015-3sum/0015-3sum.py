7b640c2d7de28d017219cca880bc8a3283fc292a
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-12 22:18:23 +0530

    Time: 487 ms (90.72%), Space: 22.2 MB (81.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,6 +1,6 @@
 """
 1. This was the famous 3-sum problem, it was a bit tough, specially skipping negatives.
-2. we are taking one fixed pointer from o to n-2 len, we have left which is fixed +1 and right at last element (one fixed element and 2 pointer method).
+2. we are taking one fixed pointer from 0 to n-2 len, we have left which is fixed +1 and right at last element (one fixed element and 2 pointer method).
 3. sort the array before we began, if sum of all 3 pos is > 0 right -- if < 0 then left ++, else append those triplet to ans and dec right and inc left.
 4. to avoid duplicates, the top loop if fixed pos > 0 and the prev fixed int is same as present one, skip it to not add those values again.
 (for eg:- [-1, -1, 0, 1]. shld be -1, 0, 1 so skip if the prev value is same in cases such as this).
@@ -35,6 +35,8 @@ class Solution:
                     right -= 1
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
                 elif curr_sum > 0:
                     right -= 1
                 else:
