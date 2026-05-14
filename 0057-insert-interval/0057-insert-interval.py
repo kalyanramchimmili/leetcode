d223341934a37c8257e57fe501be1cd99a79fffe
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-14 17:45:42 +0530

    Time: 0 ms (100%), Space: 21.4 MB (56.69%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,33 @@
+"""
+1. Tricky problem, but was easier you see how to compare
+case 1:- before the interval if last element of current interval is less than new interval no overlap it is before, append it to ans
+case 2:- if first element of current interval is greater than last element of new interval then the interval is after new interval, so append the new interval first and the current interval later
+case 3:- overlap, here we choose min of first element and max of last element to make new newInterval, to check again.
+
+if the loop is compleye append the new interval, the trickiest part is in case 2 we append new interval and we append the interval, but at last we append new interval again, we cannot break, what if new interval is not before the last interval rather it is before mid interval, we would miss rest of it.
+
+we change the newinterval to the current interval in case 2 to continue look further and we append it last anyways.
+
+time comp:- O(N)
+space comp:- O(N), N being number of intervals.
+"""
+
+
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        ans = []
+
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                ans.append(interval)
+            elif interval[0] > newInterval[1]:
+                ans.append(newInterval)
+                newInterval = interval
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+
+        ans.append(newInterval)
+        return ans
