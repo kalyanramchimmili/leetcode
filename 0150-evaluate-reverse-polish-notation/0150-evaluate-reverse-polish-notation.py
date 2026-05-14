60e218093ecfddb7b8b81a5b164c72c77b85bf9a
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-14 22:19:22 +0530

    Time: 3 ms (57.24%), Space: 20.6 MB (35.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,3 +1,13 @@
+"""
+1. The solution is using stack.
+2. If it is a number push it to stack, else if it is any operator then, we will have to pop the 2 number n1 and n2
+3. the top 2 numbers will be popped, n2 being the first and n1 being 2nd, we perform with the operators either by a switch/match case or if else
+4. append the result back to stack
+5. at last return stack[0] as the output.
+
+time comp:- O(N)
+space comp:- O(N)
+"""
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
