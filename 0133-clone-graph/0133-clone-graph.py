97c77b3017ecc151c403a6f026b9bbc6101c1f4a
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-16 14:42:07 +0530

    Time: 46 ms (79.61%), Space: 19.9 MB (18.71%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,4 +1,7 @@
 """
+
+"""
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
@@ -8,6 +11,8 @@ class Node:
 
 from typing import Optional
 class Solution:
+# using DFS
+    """
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         newmap = {}
 
@@ -25,3 +30,31 @@ class Solution:
             return newNode
         
         return clone(node)
+    """
+
+# using BFS
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        newmap = {}
+        if not node:
+            return None
+        
+        newNode = Node(node.val)
+        newmap[node] = newNode
+        q = []
+        left = 0
+        right = 0
+        q.append(node)
+        right += 1
+        while left < right:
+            curr_node = q[left]
+            left += 1
+            for i in curr_node.neighbors:
+                if i not in newmap:
+                    newNode = Node(i.val)
+                    newmap[i] = newNode
+                    q.append(i)
+                    right += 1
+
+                newmap[curr_node].neighbors.append(newmap[i])
+
+        return newmap[node]
