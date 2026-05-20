a7dba59958252e3d51a4df2c3051983a5a342367
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-20 20:56:32 +0530

    Time: 463 ms (79.52%), Space: 19.6 MB (77.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,12 +1,23 @@
+"""
+1. Learnt dp for this problem, there is still a lot to learn in dp space on how to build towards the approach. Dp is basically backtracking with memorization.
+2. how this one is solved is first a dp array is intialized with amount + 1, for comaprision, it can be float('inf') as well.
+3. dp[0] is 0, and considering for all coins begin with first coin, for dp[1] for 1 Rs, it would be 1 coing which is either min(dp[1], 1+dp[0]) i.e if 1 is not visited before it would be amount+1, so it woukd be 1 coin more than dp[0].
+4. Same thing with dp[2] etc until dp[amount], now coin 2, it will start from currAmount >= coin that index, so say for making 4 we take 4 1Rs coins, now we have a 4rs, so it shld be 1 min(4, dp[4-4]+1) i.e 1, for coin 7 etc.
+5. if sp[amount] is same as intial set value then there is no solution return -1 else return the dp value.
+
+ref:- Neetcode vedio helped for this appraoch.
+
+Time comp:- O(amount*coins) worst case:- N^2
+space comp:- O(amount)/O(N)
+"""
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1)
+        dp = [amount+1] * (amount + 1)
         dp[0] = 0
         for coin in coins:
             for currAmount in range(1, amount+1):
                 if currAmount >= coin:
                     dp[currAmount] = min(dp[currAmount], 1+dp[currAmount-coin])
-        if dp[amount] != float('inf'):
-            return dp[amount]
-        return -1
+                    
+        return dp[amount] if dp[amount] != amount+1 else -1
         
