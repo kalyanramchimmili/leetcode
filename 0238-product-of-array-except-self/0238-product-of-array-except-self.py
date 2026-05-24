6443b06d71d8174d123f0d10252523a9b5a220c5
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-24 13:51:33 +0530

    Time: 20 ms (65.13%), Space: 25.5 MB (70.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,3 +1,20 @@
+"""
+1. without division operator in o(n) time, intially though of prefix and postfix approach, which works we intialize 3 arrays prefix postfix and res.
+2. First compute prefix from range of 1 to l, prefix is prefix[i-1]*nums[i-1]
+for n = 1 2 3 4
+prefix = 1 1 2 6, for 1 no prefix def to 1, for 2 it is 1 for 3 is 2*1 and for 4 it is 3*2*1
+3. similarly for postfix, postfix is postfix[i+1]*nums[i+1], 
+for n = 1 2 3 4 -> 4 3 2 1
+postfix = 1 4 12 24, start from back for 4 its def 1 as no postfix, for 3 it is prev postfix and nums, so 4 then for 2 it is 4*3 and for 1 it is 12*2.
+For result, multiply prefix and postfix and return result multiplying both prefix and postfix.
+
+-> the optimal approach is we dont need prefix and postfix arrays, so we compute the same in res, first put prefix in res and start from opp to multiply with postfix and return the res.
+To achieve this we have prefix = 1, postfix = 1, for prefix it is 1, and prefix multiplys with the num so it becomes 1 1 2 6 and then we come from back so 6 2 1 1
+-> now 6 multiplies with 1 so 6 and for 2 it mulplies with 4 i.e nums[i] etc gives the final result.
+
+Time comp:- O(n)
+space comp:- o(1), as per the problem the output array is not added to space.
+"""
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         l = len(nums)
