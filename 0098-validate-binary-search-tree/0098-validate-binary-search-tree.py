eae62c924877bc8dcf77f5020e46dfb7d3d91021
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-05-28 14:51:15 +0530

    Time: 3 ms (35.39%), Space: 20.7 MB (95.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,3 +1,14 @@
+"""
+1. We can solve this que recursively, by check if the left node is less than the parent node and right is greater.
+2. we have a check fun with node a low and high value intialized to inf and -inf to compare.
+3. the node is comapred to see if node.left and node.right satisfy if not return False, if so continue with the loop
+4. call the fun recursively to pass in node.left and low would be -inf first, high would be node.val. Similarly for right low would be node val and high be +inf.
+5. after the first call, say 10 has 5 and 15 as left and right, and 15 as 6 as left. When 15, 10, +inf is passed, it will validate and when 15 calls 10, low remains 10 and high becomes the node val which is 15 for node.left.
+6. This condition is used to check its previous nodes as well not just the immediate previous node to validate BST.
+
+Time comp:- O(N)
+Space comp:- O(N), for the recusrive stack.
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
