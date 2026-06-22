1103067f98114a5b53cadd91a27fbaba234a1ccd
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-06-22 22:09:42 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87.02%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,26 @@
+"""
+Okay took a lot of time to get this solution click, ofc im dumb 
+1. simple dfs or backtracking, a sol array with all sol, a temp storing our trials onw.
+2. start of with 1st var say [1,2,3], start with 1, then we have either 1,2 or 1,3, say 1,2 then we append 3 if temp len is equal to actual array then append it to solution.
+3. Once we append we need to go back, when backtrack returns after appending or if dosent enter the for loop it returns none, so backtrack returns none and pops the last element, undoing the temp array one by one until 1 this case, and add 3 now and continue in that path to get 1,3,2.
+4. Intiate the backtrack fun, and return the final sol array with all possible combinations.
+
+time comp:- O(n!)
+Space comp:- O(n)
+"""
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        import itertools
-        return list(itertools.permutations(nums))
+        sol = []
+        temp = []
+        def backtrack():
+            if len(nums) == len(temp):
+                sol.append(temp[:])
+                return
 
-        
+            for num in nums:
+                if num not in temp:
+                    temp.append(num)
+                    backtrack()
+                    temp.pop()
+        backtrack()
+        return sol        
