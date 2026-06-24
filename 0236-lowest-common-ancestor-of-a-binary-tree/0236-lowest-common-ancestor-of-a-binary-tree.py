3d54dcd91fbe886547146e96c1d43f5f19948841
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-06-24 20:18:47 +0530

    Time: 133 ms (21.41%), Space: 49.2 MB (29.48%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,5 +1,17 @@
 """
-1. 
+1. dfs, so keep checking until we find the p or q or left node.
+2.  if the node is p or q return the node, if not do dfs for that node left and right. for the first ex
+        3
+       / \
+      5   1
+     / \
+    6   2
+
+for 6 would return 6, then for 2 woulld return 2 the prev dfs would get 6 and 2 for left and right so return root, the right ans here is 5 the lowest common ancestor interms of depth and not value.
+3. then dfs(3) would return 5 for left and none for right so return left or right at bottom would return 5 at end of the prog.
+
+time como:- O(N)
+space comp:- O(N)
 """
 # Definition for a binary tree node.
 # class TreeNode:
