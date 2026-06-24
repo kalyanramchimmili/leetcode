6f9dfc8e0fc5b38f36ea3b39b600ca8846718fde
KALYAN RAM CHIMMILI <101321732+kalyanramchimmili@users.noreply.github.com>
2026-06-24 19:54:29 +0530

    Time: 11 ms (24.92%), Space: 23.2 MB (16.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,3 +1,15 @@
+"""
+1. Had done a similar problem beforem but cant recall which. Checking the intervals
+2. an empty ans list, start of with first range as curr, in a for loop of 1 to l, compare 1st range with 2nd, if the start of 2nd range is smaller or equal to end of 1st range there is an overlap.
+3. if there is overlap, start would be min of both and end would be max of both, these would be curr var so it can be compared with next range.
+[[1,3],[2,6],[8,10],[15,18]], here after 1st 2 sets it would be 1,6 and now 1,6 shld compare to 8,10. 
+4. if the overlap is not there append the curr var's and start fresh by initializing curr as say 8,10. now 8,10 will compare with 15,18.
+5. at last say one case [[1,4],[4,5]], loop ends after 1,5 append it to ans and return ans.
+6. one test case fails as the interval list is not sorted so interval.sort() would sort based on 0th index element.
+
+time comp:- O(N logn) + O(N) ~ O(nlogn), n being number of intervals, nlogn for sorting.
+space comp:- O(1)
+"""
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
